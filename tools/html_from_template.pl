8f8d83ebde7995c48cadd7419635b11f166133b0
David Kaumanns <david@kaumanns.io>
2022-09-01 13:40:45 +0200

    Fixes to template tool

--- tools/html_from_template.pl
+++ tools/html_from_template.pl
@@ -1,10 +1,13 @@
-#!/usr/bin/env perl
+#!/usr/bin/env -S perl -CA
+
+# The extra flags are needed to decode @ARGS as UTF-8.
 
 use strict;
 use utf8;
 
 binmode STDIN, ":encoding(UTF-8)";
 binmode STDOUT, ":encoding(UTF-8)";
+binmode STDERR, ":encoding(UTF-8)";
 
 if ($#ARGV + 1 != 2) {
   print("Usage: cat <paths to body HTML files> | $0 <path to template HTML> <title>");
